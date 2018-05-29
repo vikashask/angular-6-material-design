92958cf7f3f169a6f68c8ecaf113e8d9f7aac0e5
vikash <vikashraj144@gmail.com>
2018-05-29 09:20:46 +0530

    form field compoent added

---
+++ src/app/components/formControls/form-field/form-field.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup,FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-form-field',
+  templateUrl: './form-field.component.html',
+  styleUrls: ['./form-field.component.css']
+})
+export class FormFieldComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  options: FormGroup;
+  constructor(fb: FormBuilder) {
+    this.options = fb.group({
+      hideRequired: false,
+      floatLabel: 'auto',
+    });
+  }
+
+  ngOnInit() {
+  }
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+
+}
