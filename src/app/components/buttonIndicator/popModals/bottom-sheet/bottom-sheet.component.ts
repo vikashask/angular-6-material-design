51e1d1a96533b3d4b5ff73936caa69680a50b40c
vikash <vikashraj144@gmail.com>
2018-07-08 16:25:39 +0530

    botton sheet compoent

---
+++ src/app/components/buttonIndicator/popModals/bottom-sheet/bottom-sheet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-sheet',
+  templateUrl: './bottom-sheet.component.html',
+  styleUrls: ['./bottom-sheet.component.css']
+})
+export class BottomSheetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
