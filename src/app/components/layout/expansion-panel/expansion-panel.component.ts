19bc95265da6e1bb5759a69e5a4111316e76dce4
vikash <vikashraj144@gmail.com>
2018-07-22 17:44:23 +0530

    expansion

---
+++ src/app/components/layout/expansion-panel/expansion-panel.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expansion-panel',
+  templateUrl: './expansion-panel.component.html',
+  styleUrls: ['./expansion-panel.component.css']
+})
+export class ExpansionPanelComponent implements OnInit {
+  panelOpenState: boolean = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
+}
