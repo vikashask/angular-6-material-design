2556bd5c6002a15f0611a17c08d0cc5d52c5459d
vikash verma <vikashraj144@gmail.com>
2018-08-22 09:02:50 +0530

    app-side-nav added

---
+++ src/app/components/navigation/side-nav/side-nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.css']
+})
+export class SideNavComponent implements OnInit {
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+  showFiller = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
